ac2e1112d8a785dbd48f930f0cc8b4039d002021
Jack Fan <jackfan@dhcp-10-250-45-102.harvard.edu>
2025-02-23 22:49:55 -0500

    Add UnionFind, indicate kruskal, lwk it lmao gl

--- randmst.py
+++ randmst.py
@@ -10,10 +10,37 @@ numpoints = int(sys.argv[2])
 numtrials = int(sys.argv[3])
 dimension = int(sys.argv[4])
 
+class TreeNode:
+    def __init__(self, x):
+        self.x = x
+        self.parent = self
+        self.rank = 0
+
 class UnionFind:
-    def __init__(self):
-        pass
+    def __init__(self): # makeset
+        self.nodes = {}
+
+    def makeset(self, x):
+        self.nodes[x] = TreeNode(x)
+
+    def union(self, x, y):
+        return self.link(self.find(self.nodes[x]), self.find(self.nodes[y]))
+
+    def find(self, x):
+        if self.nodes[x] != self.nodes[x].parent:
+            self.nodes[x].parent = self.find(self.nodes[x].parent.x)
+        return self.nodes[x].parent
      
+    def link(self, x, y):
+        if self.nodes[x].rank > self.nodes[y].rank:
+            return self.link(self.nodes[y], self.nodes[x])
+        else:
+            self.nodes[y].rank += 1
+        self.nodes[x].parent = self.nodes[y]
+        return self.nodes[y]
+
+def kruskal(g):
+    pass
 
 if dimension == 0:
     # create graph
