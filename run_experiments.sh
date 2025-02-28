8e55c322fe3b734397d7270c860ebe54c79861cd
Jack Fan <jackfan@dhcp-10-250-45-102.harvard.edu>
2025-02-28 02:40:52 -0500

    Data

--- run_experiments.sh
+++ run_experiments.sh
@@ -18,7 +18,7 @@ graph_types=("complete" "hypercube" "complete2d" "complete3d" "complete4d")
 
 # Common n values
 n_values_complete=(128 256 512 1024 2048 4096 8192 16384 32768)
-n_values_hypercube=(128 256 512 1024 2048 4096 8192 16384 65536 131072 262144)
+n_values_hypercube=(128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144)
 
 # Run experiments for each graph type
 for graph_type in "${graph_types[@]}"; do
